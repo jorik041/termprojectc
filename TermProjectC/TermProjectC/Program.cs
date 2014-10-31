c89f45c6f5023f652a3a12e0340c894b1a3aa883
Mikhail Aksenov <blabos@ya.ru>
2014-10-31 22:31:05 +0400

    First half-working version.

--- TermProjectC/TermProjectC/Program.cs
+++ TermProjectC/TermProjectC/Program.cs
@@ -6,40 +6,82 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
+using System.Threading;
+
+
 
 namespace TermProjectC
 {
     class Program
     {
+        IPAddress input_ip;
         static void Main(string[] args)
         {
-            new Server(8080);
+            int inp_port = Convert.ToInt32(Console.ReadLine());
+            int out_port = Convert.ToInt32(Console.ReadLine());
+            Server serv = new Server(inp_port);
+            Thread Thread = new Thread(new ThreadStart(serv.HandlingWithoutClosing));
+            Thread.Start();
+            Client client;
+            if (serv.GetIp() != null)
+            {
+                client = new Client(serv.GetIp(), out_port);
+            }
+            else
+            {
+                client = new Client(out_port);
+                client.Connect(IPAddress.Loopback);
+            }
+            client.SendMessage(Console.ReadLine());
         }
     }
     class Server
     {
-        TcpListener listener;
-        TcpClient client;
+        private static TcpListener listener;
+        private static TcpClient client;
+        private static IPAddress input_ip;
+        public IPAddress GetIp() { return input_ip; }
+        private static byte[] buffer;
+        private static string str;
+        public TcpClient GetClient() { return client; }
         public Server(int port)
         {
             listener = new TcpListener(IPAddress.Any, port);
             client = new TcpClient();
-            byte[] buffer = new byte [1024];
-            string str;
+            buffer = new byte [1024];
+           
+        }
+        public void Handling()
+        {            
             while (true)
             {
-                // Принимаем новых клиентов
                 listener.Start();
                 client = listener.AcceptTcpClient();
-                client.GetStream().Read(buffer,0,1024);                
+                client.GetStream().Read(buffer, 0, 1024);
                 str = Encoding.ASCII.GetString(buffer);
                 Console.WriteLine(str);
                 //Console.WriteLine("Client connected with IP {0}", client.Client.AddressFamily.);
                 Console.WriteLine(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+                input_ip = ((IPEndPoint)client.Client.RemoteEndPoint).Address;
                 client.Close();
                 listener.Stop();
             }
         }
+        public void HandlingWithoutClosing()
+        {
+             listener.Start();
+             client = listener.AcceptTcpClient();
+             while (true)
+             {
+                client.GetStream().Read(buffer, 0, 1024);
+                str = Encoding.ASCII.GetString(buffer);
+                Console.WriteLine(str);
+                //Console.WriteLine("Client connected with IP {0}", client.Client.AddressFamily.);
+                Console.WriteLine(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+                input_ip = ((IPEndPoint)client.Client.RemoteEndPoint).Address;
+             }
+        }
+        
         ~Server()
         {
             if (listener != null)
@@ -49,9 +91,32 @@ namespace TermProjectC
         }
 
     }
-    /*class Client
+    class Client
     {
-
-
-    }*/
+        private static IPAddress inp_ip;
+        TcpClient client;
+        int port;
+        public Client(IPAddress ip, int por)
+        {
+            inp_ip = ip;
+            client = new TcpClient();
+            port = por;
+        }
+        public Client(int por)
+        {
+            client = new TcpClient();
+            port = por;
+        }
+        public void SendMessage(string text)
+        {
+            client.Connect(inp_ip, port);
+            byte[] buffer = new byte[1024];
+            buffer = Encoding.ASCII.GetBytes(text);
+            client.GetStream().Write(buffer, 0, buffer.Length);
+        }
+        public void Connect(IPAddress ip)
+        {
+            inp_ip = ip;
+        }
+    }
 }
